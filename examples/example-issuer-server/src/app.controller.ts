3fb401efe692c0d6cae31fbf1ea8738a24146a46
Lukas.J Han <lukas@hopae.io>
2025-03-05 12:08:21 +0900

    feat: create nonce
    Signed-off-by: Lukas.J Han <lukas@hopae.io>

--- examples/example-issuer-server/src/app.controller.ts
+++ examples/example-issuer-server/src/app.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { NonceService } from './nonce.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly nonceService: NonceService,
+  ) {}
+
+  @Header('Cache-Control', 'no-store')
+  @Post('nonce')
+  async createNonce() {
+    return this.nonceService.createNonce();
+  }
 }
