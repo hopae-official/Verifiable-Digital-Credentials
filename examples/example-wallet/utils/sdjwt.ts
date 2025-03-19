f7684bc848f7a71e6b5f9e28dfe056c950540033
Lukas.J.Han <lukas.j.han@gmail.com>
2025-03-19 09:56:28 +0900

    feat: add decode sdjwt utility function
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

---
+++ examples/example-wallet/utils/sdjwt.ts
@@ -0,0 +1,21 @@
+import { decodeSdJwtSync, getClaimsSync } from '@sd-jwt/decode';
+import { hasher } from '@sd-jwt/hash';
+
+/**
+ * Decodes a base64url encoded SD-JWT
+ *
+ * @param sdjwt The base64url encoded SD-JWT to decode
+ * @returns header and claims(processed payloads)
+ */
+export const decodeSDJWT = (sdjwt: string) => {
+  const decoded = decodeSdJwtSync(sdjwt, hasher);
+  const header = decoded.jwt.header;
+
+  const claims = getClaimsSync(
+    decoded.jwt.payload,
+    decoded.disclosures,
+    hasher,
+  );
+
+  return { header, claims };
+};
