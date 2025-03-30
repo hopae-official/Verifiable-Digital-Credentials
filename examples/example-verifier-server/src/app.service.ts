ff80e90232d1289c3dbe729351e409f26ea02a64
Lukas.J.Han <lukas.j.han@gmail.com>
2025-03-30 23:59:11 +0900

    feat: prep for verifier API
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- examples/example-verifier-server/src/app.service.ts
+++ examples/example-verifier-server/src/app.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class AppService {
   private readonly verifierUri: string;
+  private readonly statusMap: Map<string, Record<string, unknown>> = new Map();
   constructor(private readonly configService: ConfigService) {
     this.verifierUri = this.configService.get<string>('VERIFIER_URI');
   }
@@ -11,16 +13,17 @@ export class AppService {
   start(inputType: string) {
     const type =
       inputType === 'job-application' ? 'job-application' : 'telecom-register';
+    const id = randomUUID();
     const query = new URLSearchParams({
       client_id: encodeURI(`redirect_uri:${this.verifierUri}`),
-      request_uri: encodeURI(`${this.verifierUri}/request/${type}`),
+      request_uri: encodeURI(`${this.verifierUri}/request/${type}/${id}`),
       request_uri_method: 'post',
     });
 
-    return { link: `openid4vp://?${query.toString()}` };
+    return { link: `openid4vp://?${query.toString()}`, id };
   }
 
-  request(type: string) {
+  request(type: string, id: string) {
     const dcql_query =
       type === 'job-application'
         ? JSON.stringify({
@@ -34,7 +37,7 @@ export class AppService {
 
     const result = {
       client_id: `redirect_uri:${this.verifierUri}`,
-      response_uri: `${this.verifierUri}/response`,
+      response_uri: `${this.verifierUri}/response/${id}`,
       response_type: 'vp_token',
       response_mode: 'direct_post',
       nonce: 'n-0S6_WzA2Mj',
@@ -44,8 +47,18 @@ export class AppService {
     return result;
   }
 
-  response(vp_token: Record<string, unknown>) {
+  response(id: string, vp_token: Record<string, unknown>) {
     console.log('vp_token', vp_token);
+    // TODO: fix the data
+    this.statusMap.set(id, vp_token);
     return true;
+  }
+
+  status(id: string) {
+    const data = this.statusMap.get(id);
+    if (data) {
+      return { status: 'success', ...data };
+    }
+    return { status: 'pending' };
   }
 }
