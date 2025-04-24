567953b8feb86c72aad380925db375977cec931e
Lukas.J Han <lukas@hopae.io>
2025-04-24 18:55:56 +0900

    feat: make it simpler
    Signed-off-by: Lukas.J Han <lukas@hopae.io>

--- packages/oid4vci/src/oid4vci.service.ts
+++ packages/oid4vci/src/oid4vci.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Oid4VciOptions } from './type';
-import { CredentialProvider } from './iservice';
 
 @Injectable()
 export class Oid4VciService {
-  constructor(
-    private readonly options: Oid4VciOptions,
-    private readonly credentialProvider: CredentialProvider,
-  ) {}
+  constructor(private readonly options: Oid4VciOptions) {}
 }
--- packages/oid4vci/src/type.ts
+++ packages/oid4vci/src/type.ts
@@ -1,25 +1,17 @@
 import { Type } from '@nestjs/common';
-import { CredentialProvider } from './iservice';
 
-export type Oid4VciModuleOptions = {
-  credentialProvider?: Type<CredentialProvider>;
-  options: Oid4VciOptions;
-};
+export class Oid4VciOptions {
+  credential_issuer: string;
+}
 
 export type Oid4VciModuleAsyncOptions = {
   imports?: any[];
   useExisting?: Type<Oid4VciOptionsFactory>;
   useClass?: Type<Oid4VciOptionsFactory>;
-  useFactory?: (
-    ...args: any[]
-  ) => Promise<Oid4VciModuleOptions> | Oid4VciModuleOptions;
+  useFactory?: (...args: any[]) => Promise<Oid4VciOptions> | Oid4VciOptions;
   inject?: any[];
 };
 
 export type Oid4VciOptionsFactory = {
-  createOid4VciOptions(): Promise<Oid4VciModuleOptions> | Oid4VciModuleOptions;
+  createOid4VciOptions(): Promise<Oid4VciOptions> | Oid4VciOptions;
 };
-
-export class Oid4VciOptions {
-  credential_issuer: string;
-}
