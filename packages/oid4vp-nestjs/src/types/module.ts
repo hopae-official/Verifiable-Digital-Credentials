dba5630e129c4036ab907e15a0757924c5bcc0f9
Lukas.J.Han <lukas.j.han@gmail.com>
2025-05-11 22:07:39 +0900

    fix: build error
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- packages/oid4vp-nestjs/src/types/module.ts
+++ packages/oid4vp-nestjs/src/types/module.ts
@@ -1,1 +1,15 @@
-export type Oid4VpOptions = {};
+import { Type } from '@nestjs/common';
+
+export class Oid4VpOptions {}
+
+export interface Oid4VpOptionsFactory {
+  createOid4VpOptions(): Promise<Oid4VpOptions> | Oid4VpOptions;
+}
+
+export type Oid4VpModuleAsyncOptions = {
+  imports?: any[];
+  useExisting?: Type<Oid4VpOptionsFactory>;
+  useClass?: Type<Oid4VpOptionsFactory>;
+  useFactory?: (...args: any[]) => Promise<Oid4VpOptions> | Oid4VpOptions;
+  inject?: any[];
+};
