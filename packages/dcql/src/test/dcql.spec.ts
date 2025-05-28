f94775d6cec5bae604ab4afc53e5e9ac3f5df226
Lukas.J Han <lukas@hopae.io>
2025-05-28 11:42:22 +0900

    fix: lint
    Signed-off-by: Lukas.J Han <lukas@hopae.io>

--- packages/dcql/src/test/dcql.spec.ts
+++ packages/dcql/src/test/dcql.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { DCQL } from '../dcql';
 import { SdJwtVcCredential } from '../credentials/sdjwtvc.credential';
 import { Claims, CredentialSet, rawDCQL } from '../type';
